deb3889991bf1a3ef98abe53e49effacf2a762a2
TheWoodenMan <andynwood@googlemail.com>
2022-10-23 22:54:21 +0100

    vowelsCounter

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,42 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
+// Chaining in-built Methods
+// You can start with .split() or [...text] which is .2ms faster.
+// function reverseString(text) {
+// 	return [...text].reverse().join("");
+// }
 
+// For loop
+//  n.b. the loop decrements - that is it starts with the last character and iterates backwards as long as i >= 0
 
-function reverseString(text) {
-    // Code goes here
-}
+// function reverseString(text) {
+// 	let outputString = "";
+// 	for (let i = text.length - 1; i >= 0; i--) {
+// 		outputString += text[i];
+// 	}
+// 	return outputString;
+// }
 
+// different way to do it which reads better but is actually 0.1ms slower
 
+// function reverseString(text) {
+// 	let outputString = "";
+// 	for (let char of text) {
+// 		outputString = char + outputString;
+// 	}
+// 	return outputString;
+// }
 
-module.exports = reverseString
+// Recursion
+//
+
+// function reverseString(text) {
+// 	if (text === "") {
+// 		return "";
+// 	} else {
+// 		return reverseString(text.substr(1)) + text[0];
+// 	}
+// }
+
+// // Reduce@
--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,11 +3,29 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+// Iterative Approach O(n)
 
-function vowelsCounter(text) {
-    // Code goes here
-}
+// function vowelsCounter(text) {
+// 	const vowels = ["a", "e", "i", "o", "u"];
+// 	let counter = 0;
+// 	for (let letter of text.toLowerCase()) {
+// 		if (vowels.includes(letter)) {
+// 			counter++;
+// 		}
+// 	}
+// 	return counter;
+// }
 
+// regex approach O(1)
 
+// function vowelsCounter(text) {
+// 	let matches = text.match(/[aeiou]/gi);
+
+// 	if (matches) {
+// 		return matches.length;
+// 	} else {
+// 		return 0;
+// 	}
+// }
 
 module.exports = vowelsCounter;
